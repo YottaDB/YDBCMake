8b2c26c20a4ee9877fb157bc7e9a9c08e19a1d06
Sam Habiel <sam@yottadb.com>
2021-10-21 12:19:43 -0600

    Revert "Add YottaDB copyrights to all files in repo"
    This reverts commit 9d5fffb56210df31ed8fc113d81b6e817882bcf8.
    
    The files here are dedicated to being public domain in order to avoid
    copyright/license pollution into downstream projects not owned by
    YottaDB. Adding a copyright was a mistake; and it's now reverted.

--- ydbcmake/FindYOTTADB.cmake
+++ ydbcmake/FindYOTTADB.cmake
@@ -1,15 +1,3 @@
-#################################################################
-#								#
-# Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	#
-# All rights reserved.						#
-#								#
-#	This source code contains the intellectual property	#
-#	of its copyright holder(s), and is made available	#
-#	under a license.  If you do not know the terms of	#
-#	the license, please stop and do not read further.	#
-#								#
-#################################################################
-
 # This is free and unencumbered software released into the public domain.
 #
 # Anyone is free to copy, modify, publish, use, compile, sell, or
