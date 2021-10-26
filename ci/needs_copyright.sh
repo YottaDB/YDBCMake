b2ebe2145f634f03492fd07d6be33efe4e153fac
Sam Habiel <sam@yottadb.com>
2021-10-26 11:46:40 -0600

    Add Gitlab CI to do commit verify

---
+++ ci/needs_copyright.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+set -eu
+
+# No copyright needed on any file in this repo
+exit 0
